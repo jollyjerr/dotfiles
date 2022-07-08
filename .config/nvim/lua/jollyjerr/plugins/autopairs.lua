d9177cc88e4adf18bfea003228c69bd17d40b780
Jeremiah Tabb <jollyjerr@gmail.com>
2022-07-08 00:06:22 -0600

    fix autopairs

--- .config/nvim/lua/jollyjerr/plugins/autopairs.lua
+++ .config/nvim/lua/jollyjerr/plugins/autopairs.lua
@@ -1,15 +1,8 @@
-local remap = vim.api.nvim_set_keymap
-local npairs = require('nvim-autopairs')
-npairs.setup({map_cr=false})
+require('nvim-autopairs').setup()
 
-_G.MUtils= {}
-
-MUtils.completion_confirm=function()
-  if vim.fn.pumvisible() ~= 0  then
-    return vim.fn["coc#_select_confirm"]()
-  else
-    return npairs.autopairs_cr()
-  end
-end
-
-remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
