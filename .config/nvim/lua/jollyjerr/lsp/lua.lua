ecff9ac82e3251a86213b935862912d5a56c67af
Jeremiah Tabb <jollyjerr@gmail.com>
2023-02-13 09:17:41 -0700

    update to lua_ls

--- .config/nvim/lua/jollyjerr/lsp/lua.lua
+++ .config/nvim/lua/jollyjerr/lsp/lua.lua
@@ -1,6 +1,6 @@
 local defaults = require('jollyjerr.modules.lsp')
 
-require('lspconfig').sumneko_lua.setup({
+require('lspconfig').lua_ls.setup({
     on_attach = defaults.on_attach,
     capabilities = defaults.get_capabilities(),
     settings = {
