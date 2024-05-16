372b02c651b159421e300ef6815bc06df5250ad9
Jeremiah Tabb <jollyjerr@gmail.com>
2024-05-15 21:53:15 -0600

    clean

--- nvim/.config/nvim/lua/jollyjerr/plugins/tabby.lua
+++ nvim/.config/nvim/lua/jollyjerr/plugins/tabby.lua
@@ -5,7 +5,6 @@ return {
     config = function()
         local theme = {
             fill = 'TabLineFill',
-            -- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
             head = 'TabLine',
             current_tab = 'TabLineSel',
             tab = 'TabLine',
