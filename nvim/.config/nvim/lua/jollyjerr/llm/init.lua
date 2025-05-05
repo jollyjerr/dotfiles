14515152aa3516fc0450de7feca83c11b1265dc8
Jeremiah Tabb <jollyjerr@gmail.com>
2025-05-05 10:42:47 -0600

    start figuring out the commands I really want

--- nvim/.config/nvim/lua/jollyjerr/llm/init.lua
+++ nvim/.config/nvim/lua/jollyjerr/llm/init.lua
@@ -1,6 +1,6 @@
-vim.api.nvim_set_keymap('n', '<space>ll', ':lua SendBufferToLlm()<CR>', { noremap = true, silent = true })
+local M = {}
 
-function _G.SendBufferToLlm()
+function M.llm_chat_with_buffer_context()
     local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
     local content = table.concat(lines, '\n')
 
@@ -12,3 +12,22 @@ function _G.SendBufferToLlm()
 
     vim.fn.system(tmux_cmd)
 end
+
+function M.llm_chat_with_yank_context()
+    local yank_content = vim.fn.getreg('"')
+
+    local system = "Consider this context when responding:" .. yank_content .. ""
+    local escaped_system = vim.fn.shellescape(system)
+    local window_cmd = string.format("llm chat -s %s", escaped_system)
+    local escaped_window = vim.fn.shellescape(window_cmd)
+    local tmux_cmd = string.format("tmux split-window -p 40 -h %s", escaped_window)
+
+    vim.fn.system(tmux_cmd)
+end
+
+local keymap = vim.keymap.set
+
+keymap('n', '<leader>llb', M.llm_chat_with_buffer_context, { noremap = true, silent = true })
+keymap('n', '<leader>lly', M.llm_chat_with_yank_context, { noremap = true, silent = true })
+
+return M
